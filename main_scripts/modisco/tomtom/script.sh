1764f83aa8ea86c9884aa4fb2e4ff5ec464c0ae7
Anusri Pampari <p.anushri25@gmail.com>
2021-07-25 15:32:08 -0700

    modisco changes for bias

--- main_scripts/modisco/tomtom/script.sh
+++ main_scripts/modisco/tomtom/script.sh
@@ -5,5 +5,15 @@ tomtom_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/tomtom/SIGNAL
 #bash run_tomtom.sh GM12878 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
 #bash run_tomtom.sh HEPG2 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
 #bash run_tomtom.sh IMR90 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
-bash run_tomtom.sh H1 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
+#bash run_tomtom.sh H1 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
+
+
+input_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/importance_scores/BIAS/
+output_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/modisco/BIAS/
+tomtom_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/tomtom/BIAS/
+
+#bash run_tomtom.sh GM12878 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
+#bash run_tomtom.sh HEPG2 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
+bash run_tomtom.sh IMR90 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
+#bash run_tomtom.sh H1 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
 
