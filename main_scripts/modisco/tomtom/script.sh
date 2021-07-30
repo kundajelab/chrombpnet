e18429ca73e6b245ff9aad40653636cd48527831
Anusri Pampari <p.anushri25@gmail.com>
2021-07-29 23:24:04 -0700

    modisco run changes

--- main_scripts/modisco/tomtom/script.sh
+++ main_scripts/modisco/tomtom/script.sh
@@ -2,19 +2,20 @@ flank=500
 input_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/importance_scores/SIGNAL/
 output_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/modisco/flank_len_$flank/SIGNAL/
 tomtom_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/tomtom/flank_len_$flank/SIGNAL/
+model_name=tobias_ATAC_07.26.2021
 
-bash run_tomtom.sh GM12878 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
-bash run_tomtom.sh HEPG2 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
-bash run_tomtom.sh IMR90 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
-bash run_tomtom.sh H1 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
+bash run_tomtom.sh GM12878 $model_name $input_dir $output_dir $tomtom_dir
+bash run_tomtom.sh HEPG2 $model_name $input_dir $output_dir $tomtom_dir
+bash run_tomtom.sh IMR90 $model_name $input_dir $output_dir $tomtom_dir
+bash run_tomtom.sh H1 $model_name $input_dir $output_dir $tomtom_dir
 
 
-input_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/importance_scores/BIAS/
-output_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/modisco/flank_len_$flank/BIAS/
-tomtom_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/tomtom/flank_len_$flank/BIAS/
+#input_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/importance_scores/BIAS/
+#output_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/modisco/flank_len_$flank/BIAS/
+#tomtom_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/tomtom/flank_len_$flank/BIAS/
 
-bash run_tomtom.sh GM12878 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
-bash run_tomtom.sh HEPG2 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
-bash run_tomtom.sh IMR90 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
-bash run_tomtom.sh H1 ATAC_07.22.2021 $input_dir $output_dir $tomtom_dir
+#bash run_tomtom.sh GM12878 $model_name $input_dir $output_dir $tomtom_dir
+#bash run_tomtom.sh HEPG2 $model_name $input_dir $output_dir $tomtom_dir
+#bash run_tomtom.sh IMR90 $model_name $input_dir $output_dir $tomtom_dir
+#bash run_tomtom.sh H1 $model_name $input_dir $output_dir $tomtom_dir
 
