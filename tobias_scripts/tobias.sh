bf190b84d719193b69aa06c4b8343dd2377f3dd5
Anusri Pampari <p.anushri25@gmail.com>
2021-09-06 15:08:37 -0700

    restructuring code

--- tobias_scripts/tobias.sh
+++
@@ -1,8 +0,0 @@
-in_bam=$1
-genome_fasta=$2
-merged_overlap=$3
-blacklist=$4
-outdir=$5
-
-TOBIAS ATACorrect --bam $in_bam --genome $genome_fasta --peaks $merged_overlap --blacklist $blacklist --outdir $outdir --cores 8 --read_shift 4 -4
-
