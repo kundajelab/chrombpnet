2b5327c3df45c0b785407cce8da143be157065da
Anusri Pampari <p.anushri25@gmail.com>
2021-09-08 11:58:39 -0700

    updated loss weights path

--- src/models/chrombpnet_scripts/get_loss_weights.sh
+++ src/models/chrombpnet_scripts/get_loss_weights.sh
@@ -6,7 +6,7 @@ task=$5
 flank_size=$6
 out_file_name=$7
 
-python $PWD/main_scripts/get_loss_weights_for_bpnet.py --tdb_array $tdb_array \
+python $PWD/src/models/chrombpnet_scripts/get_loss_weights_for_bpnet.py --tdb_array $tdb_array \
                             --chroms $chroms \
                             --upsample_attribute $ambig_attribute \
                             --label_attribute $label_attribute \
