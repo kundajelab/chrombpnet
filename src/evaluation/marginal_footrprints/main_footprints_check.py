a3e68971e220ee511491c9d142d88d26657171ce
Anusri Pampari <p.anushri25@gmail.com>
2021-09-15 13:06:10 -0700

    converted notebooks to scripts

--- src/evaluation/marginal_footrprints/main_footprints_check.py
+++ src/evaluation/marginal_footrprints/main_footprints_check.py
@@ -8,25 +8,31 @@ import one_hot
 import random
 import pickle as pkl
 import matplotlib.pyplot as plt
+import argparse
+
+parser=argparse.ArgumentParser(description="wrapper for marginal footprinting")
+parser.add_argument("--gc_neg")
+parser.add_argument("--ref_fasta")
+parser.add_argument("--model_dir")
+parser.add_argument("--motifs")
+args = parser.parse_args() 
 
 random.seed(1)
 
+reference_genome_path=args.ref_fasta
+gc_matched_negs = args.gc_neg
+path=args.model_dir
 
-reference_genome_path="/mnt/data/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta"
-gc_matched_negs = "/srv/scratch/anusri/chrombpnet_paper/H1/negatives_data/bpnet.inputs.all.negatives.bed"
 
 reader = pyfaidx.Fasta(reference_genome_path)
 neg_bed=pd.read_csv(gc_matched_negs,delimiter="\t", names=["chr", "start", "end", "name", "score", "strand", "pval", "qval", "qval2", "summit"])
 neg_bed.head()
-
-path="/srv/scratch/anusri/chrombpnet_paper/GM12878/ATAC_07.22.2021/final_model_step3_new/unplug/"
 
 try:
     model=load_model_wrapper(model_hdf5=path+"model.0.hdf5")
 except:
     model=load_model_wrapper(json_string=path+"model.0.arch", weights=path+"model.0.weights")
 
-output_file="hintatac_tn5_motif2.pkl"
 test_nonpeaks = neg_bed[(neg_bed["chr"]=="chr1")]
 
 
@@ -43,8 +49,8 @@ def softmax(x, temp=1):
     return np.exp(temp*norm_x)/np.sum(np.exp(temp*norm_x), axis=1, keepdims=True)
 
 test_nonpeaks_seqs = get_seq(reader, test_nonpeaks)
-
 num=test_nonpeaks_seqs.shape[0]
+print(num)
 
 def profiles_for_motif(seqs, motif, model_peaks_corrected):
     random.shuffle(seqs)
@@ -62,20 +68,31 @@ def profiles_for_motif(seqs, motif, model_peaks_corrected):
     return predictions
 
 
-#motifs_to_test = ["GCACAGTACAGAGCTG", "GTGCACAGTTCTAGAGTGTGCAG", "CCTCTACACTGTGCAGAA", "GCACAGTTCTAGACTGTGCAG", "CTGCACAGTGTAGAGTTGTGC"]
-#Tn5
+if args.motifs == "tn5":
+    motifs_to_test = ["GCACAGTACAGAGCTG", "GTGCACAGTTCTAGAGTGTGCAG", "CCTCTACACTGTGCAGAA", "GCACAGTTCTAGACTGTGCAG", "CTGCACAGTGTAGAGTTGTGC"]
+    ylims=[0.06,0.06,0.06,0.06, 0.06] 
+    #Tn5
+elif args.motifs == "tn5_c":
+    motifs_to_test = ["GCACAGTACAGAGCTG", "GTGCACAGTTCTAGAGTGTGCAG", "CCTCTACACTGTGCAGAA", "GCACAGTTCTAGACTGTGCAG", "CTGCACAGTGTAGAGTTGTGC"]
+    ylims=[0.0015,0.0015,0.0015,0.0015, 0.0015] 
+elif args.motifs == "dnase1":
+    motifs_to_test = ["TTTACAAGTCCA", "TGTACTTACGAA"]
+    #DNASE
+elif args.motifs == "gm12878_motifs_set1":
+    motifs_to_test = ["GCGCATGCGC", "CGATATGACTCATCCC", "TTGGCCACTAGGGGGCGCTAT", "CCGAAAGCGGAAGTGAGAC"]
+    ylims=[0.004,0.003,0.02,0.004] 
+    #NRF1, AP1, CTCF, ETS - GM12878
+elif args.motifs == "gm12878_motifs_set2":
+    motifs_to_test =  ["AAGGGGGCGGGGCCTAA", "CCCTAACCACAGCCC", "GCAAGGGAAATTCCCCAGG", "GATGG"]
+    ylims=[0.002,0.002,0.003,0.002] 
 
-#motifs_to_test = ["TTTACAAGTCCA", "TGTACTTACGAA"]
-#DNASE
-
-motifs_to_test = ["GCGCATGCGC", "CGATATGACTCATCCC", "TTGGCCACTAGGGGGCGCTAT", "CCGAAAGCGGAAGTGAGAC"]
-#NRF1, AP1, CTCF, ETS - GM12878
+    #SPI1, RUNX, NFKB, GATA1
+else:
+    pass
 
 #motifs_to_test = ["ATAGCGCCCCCTAGTGGCCAA", "TTGGCCACTAGGGGGCGCTAT"]
 #motifs_to_test = ["CCATTGTTATGCAAAT"]
 
-#motifs_to_test = ["AAGGGGGCGGGGCCTAA", "CCCTAACCACAGCCC", "GCAAGGGAAATTCCCCAGG", "GATGG"]
-#SPI1, RUNX, NFKB, GATA1
 
 predictions_motifs = []
 for motif in motifs_to_test:
@@ -85,23 +102,20 @@ for motif in motifs_to_test:
 def plot_tracks(pred_unplug_bias, ax=None, ylim=0.01, start=500-100+5, end=500+100+5 ):
     width = end - start
     ax.plot(range(width), pred_unplug_bias[:, start:end].mean(0))
-    #ax.set_ylim(0,ylim)   
+    ax.set_ylim(0,ylim)   
     #plt.legend()
 
-plt.rcParams["figure.figsize"] = (28,4)
+plt.rcParams["figure.figsize"] = (7*len(motifs_to_test),4)
 fig, axs = plt.subplots(1, len(motifs_to_test))
-
-
-#ylims=[0.002,0.002,0.002,0.002, 0.002] 
  
 i=0
 for pred_unplug_bias in predictions_motifs:
-    plot_tracks(np.array(pred_unplug_bias).reshape(num,1000), axs[i])
+    plot_tracks(np.array(pred_unplug_bias).reshape(num,1000), axs[i], ylim=ylims[i])
     i+=1
-    
-plt.show()
 
-p#kl.dump(pred_unplug_bias, open(output_file, "wb"))
+plt.savefig(os.path.join(path,args.motifs+"_footprint.png"))
+#plt.show()
+#p#kl.dump(pred_unplug_bias, open(output_file, "wb"))
 
 
 
--- src/evaluation/modisco/tomtom/script.sh
+++ src/evaluation/modisco/tomtom/script.sh
@@ -2,42 +2,27 @@ flank=500
 input_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/importance_scores/SIGNAL/
 output_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/modisco/flank_len_$flank/SIGNAL/
 tomtom_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/tomtom/flank_len_$flank/SIGNAL/
-#model_name=tobias_ATAC_07.26.2021
-#model_name=naked_bias_4_4_shifted_ATAC_07.29.2021
-#model_name=4_5_shifted_ATAC_07.28.2021
-#model_name=tobias_ATAC_08.03.2021
-#model_name=ATAC_07.22.2021_new
-#model_name=naked_bias_4_4_shifted_ATAC_07.29.2021_new
-#model_name=4_4_shifted_ATAC_08.19.2021_bias_filters_100_new
-#model_name=4_4_shifted_ATAC_08.19.2021_bias_filters_250_new
-#model_name=4_4_shifted_ATAC_08.19.2021_bias_filters_50_new
-#model_name=DNASE_08.19.2021_bias_filters_250_new
-#model_name=DNASE_08.19.2021_bias_filters_50_new
-#model_name=ATAC_07.22.2021_with_H1_bias
-#model_name=4_4_shifted_ATAC_08.12.2021_new
-#model_name=naked_bias_4_4_shifted_DNASE_08.19.2021_new
-model_name=4_4_shifted_ATAC_08.12.2021_with_gm12878_bias
 
-bash run_tomtom.sh SURAG $model_name $input_dir $output_dir $tomtom_dir
+model_name=4_4_shifted_ATAC_09.12.2021_bias_filters_500_new
 
+#bash run_tomtom.sh SURAG $model_name $input_dir $output_dir $tomtom_dir
 #bash run_tomtom.sh GM12878 $model_name $input_dir $output_dir $tomtom_dir
 #bash run_tomtom.sh HEPG2 $model_name $input_dir $output_dir $tomtom_dir
 #bash run_tomtom.sh IMR90 $model_name $input_dir $output_dir $tomtom_dir
 #bash run_tomtom.sh H1 $model_name $input_dir $output_dir $tomtom_dir
+#bash run_tomtom.sh K562 $model_name $input_dir $output_dir $tomtom_dir
 
 
 input_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/importance_scores/BIAS/
 output_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/modisco/flank_len_$flank/BIAS/
 tomtom_dir=/oak/stanford/groups/akundaje/projects/chrombpnet_paper/tomtom/flank_len_$flank/BIAS/
 
-#model_name=ATAC_07.22.2021_new_uncorrected
-#model_name=ATAC_07.22.2021_biasfit
-#model_name=4_4_shifted_ATAC_uncorrected_experimental_counts_var2_08.20.2021
-#model_name=naked_bias_4_4_shifted_DNASE_08.19.2021
+model_name=4_4_shifted_ATAC_09.12.2021_bias_filters_500
 
 #bash run_tomtom.sh naked_dna $model_name $input_dir $output_dir $tomtom_dir
 #bash run_tomtom.sh GM12878 $model_name $input_dir $output_dir $tomtom_dir
 #bash run_tomtom.sh HEPG2 $model_name $input_dir $output_dir $tomtom_dir
 #bash run_tomtom.sh IMR90 $model_name $input_dir $output_dir $tomtom_dir
 #bash run_tomtom.sh H1 $model_name $input_dir $output_dir $tomtom_dir
+bash run_tomtom.sh K562 $model_name $input_dir $output_dir $tomtom_dir
 
