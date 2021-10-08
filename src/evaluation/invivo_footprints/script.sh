3803afb1af54a36b289c66623b7d6807c1909d3e
Anusri Pampari <p.anushri25@gmail.com>
2021-10-08 14:01:50 -0700

    invivo footprinting from Alex

---
+++ src/evaluation/invivo_footprints/script.sh
@@ -0,0 +1,4 @@
+python tf_modiscohits.py --outdir=/srv/scratch/anusri/chrombpnet_paper/src/evaluation/invivo_footprints/outputs/ \
+	/srv/scratch/anusri/chrombpnet_paper/results/chrombpnet/ATAC/K562/4_4_shifted_ATAC_09.29.2021_bias_filters_500/final_model_step3/unplug/deepshap/20K.fold0.deepSHAP \
+	/oak/stanford/groups/akundaje/projects/chrombpnet_paper/modisco/flank_len_500/SIGNAL/K562/4_4_shifted_ATAC_09.29.2021_bias_filters_500_new/20K.fold0.deepSHAP.profile_shap.hdf5 \
+	/srv/scratch/anusri/chrombpnet_paper/results/chrombpnet/ATAC/K562/data/K562_idr_split/combine.bed \
