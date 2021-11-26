8328a1f169edfc8b219688ff23205a1a9cc24908
Anusri Pampari <p.anushri25@gmail.com>
2021-11-26 00:06:42 -0800

    moved this to needs cleanup added new way of model loading

--- src/models/hint_atac_scripts/logs/SEPT_09/hint_atac.sh
+++
@@ -1,7 +0,0 @@
-cell_line=$1
-in_bam=$2
-merged_overlap=$3
-
-rgt-hint tracks --bc --bigWig --organism=hg38 $in_bam $merged_overlap  --output-prefix=$PWD/hint_atac_scripts/$cell_line/bias_corrected --forward-shift 4 --reverse-shift -4
-
-
--- src/models/hint_atac_scripts/logs/SEPT_09/hint_dnase.sh
+++
@@ -1,8 +0,0 @@
-
-cell_line=$1
-in_bam=$2
-merged_overlap=$3
-
-rgt-hint tracks --bc --bigWig --organism=hg38 $in_bam $merged_overlap  --bias-table /users/anusri/rgtdata/fp_hmms/double_hit_bias_table_F.txt,/users/anusri/rgtdata/fp_hmms/double_hit_bias_table_R.txt --output-prefix=$PWD/hint_atac_scripts/$cell_line/bias_corrected_dnase --forward-shift 0 --reverse-shift +1
-
-
