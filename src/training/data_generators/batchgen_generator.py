28c356803d3542e4240152b4d2e398f31c307049
Anusri Pampari <p.anushri25@gmail.com>
2021-11-28 21:20:21 -0800

    removed tiledb, generator name, added shuffle as switch param

--- src/training/data_generators/batchgen_generator.py
+++ src/training/data_generators/batchgen_generator.py
@@ -1,6 +1,6 @@
 from tensorflow import keras
-import utils.batchgen_generator_augment as augment
-import utils.batchgen_generator_utils as utils
+from utils import augment
+from utils import data_utils
 import tensorflow as tf
 import numpy as np
 import random
@@ -26,7 +26,7 @@ class ChromBPNetBatchGenerator(keras.utils.Sequence):
     every epoch, and calls bias model on it, whose outputs (bias profile logits 
     and bias logcounts) are fed as input to the chrombpnet model.
     """
-    def __init__(self, peak_regions, nonpeak_regions, genome_fasta, batch_size, inputlen, outputlen, max_jitter, negative_sampling_ratio, cts_sum_min_thresh, cts_sum_max_thresh, cts_bw_file, seed, add_revcomp, return_coords):
+    def __init__(self, peak_regions, nonpeak_regions, genome_fasta, batch_size, inputlen, outputlen, max_jitter, negative_sampling_ratio, cts_sum_min_thresh, cts_sum_max_thresh, cts_bw_file, seed, add_revcomp, return_coords, shuffle_at_epoch_start):
         """
         seqs: B x L' x 4
         cts: B x M'
@@ -39,7 +39,7 @@ class ChromBPNetBatchGenerator(keras.utils.Sequence):
         random.seed(seed)
         tf.random.set_seed(seed)
 
-        peak_seqs, peak_cts, peak_coords, nonpeak_seqs, nonpeak_cts, nonpeak_coords, = utils.load_data(peak_regions, nonpeak_regions, genome_fasta, cts_bw_file, inputlen, outputlen, max_jitter, cts_sum_min_thresh, cts_sum_max_thresh)
+        peak_seqs, peak_cts, peak_coords, nonpeak_seqs, nonpeak_cts, nonpeak_coords, = data_utils.load_data(peak_regions, nonpeak_regions, genome_fasta, cts_bw_file, inputlen, outputlen, max_jitter, cts_sum_min_thresh, cts_sum_max_thresh)
         self.peak_seqs, self.nonpeak_seqs = peak_seqs, nonpeak_seqs
         self.peak_cts, self.nonpeak_cts = peak_cts, nonpeak_cts
         self.peak_coords, self.nonpeak_coords = peak_coords, nonpeak_coords
@@ -50,6 +50,7 @@ class ChromBPNetBatchGenerator(keras.utils.Sequence):
         self.batch_size = batch_size
         self.add_revcomp = add_revcomp
         self.return_coords = return_coords
+        self.shuffle_at_epoch_start = shuffle_at_epoch_start
         self.seed=seed
 
 
@@ -86,21 +87,16 @@ class ChromBPNetBatchGenerator(keras.utils.Sequence):
         else :
             print("Both peak and non-peak arrays are empty")
 
-
-        if self.add_revcomp:
-            self.cur_seqs, self.cur_cts, self.cur_coords = augment.crop_revcomp_augment(
-                                             self.seqs, self.cts, self.coords, self.inputlen, self.outputlen, 
-                                             seed=self.seed, shuffle=True
+        self.cur_seqs, self.cur_cts, self.cur_coords = augment.crop_revcomp_augment(
+                                            self.seqs, self.cts, self.coords, self.inputlen, self.outputlen, 
+                                            self.add_revcomp, seed=self.seed, shuffle=self.shuffle_at_epoch_start
                                           )
-        else:
-            self.cur_seqs, self.cur_cts, self.cur_coords = self.seqs, self.cts, self.coords
-
 
     def __getitem__(self, idx):
         batch_seq = self.cur_seqs[idx*self.batch_size:(idx+1)*self.batch_size]
         batch_cts = self.cur_cts[idx*self.batch_size:(idx+1)*self.batch_size]
         batch_coords = self.cur_coords[idx*self.batch_size:(idx+1)*self.batch_size]
-        
+
         if self.return_coords:
             return (batch_seq, [np.expand_dims(batch_cts,axis=2), np.log(1+batch_cts.sum(-1, keepdims=True))], batch_coords)
         else:
--- src/training/data_generators/initializers.py
+++ src/training/data_generators/initializers.py
@@ -1,48 +1,58 @@
-import generators.tiledb_generator as tiledb_generator
-import generators.batchgen_generator as batchgen_generator
-import utils.batchgen_generator_utils as data_utils
+import data_generators.batchgen_generator as batchgen_generator
+from utils import data_utils
 import pandas as pd
 import splits
 
 NARROWPEAK_SCHEMA = ["chr", "start", "end", "1", "2", "3", "4", "5", "6", "summit"]
 
-def fetch_values_based_on_mode(mode, args, parameters, nonpeak_regions):
+def fetch_params_based_on_mode(mode, args, parameters, nonpeak_regions):
+
     if mode=="train":
-        cts_sum_min_thresh=parameters["cts_sum_min_thresh"]
-        cts_sum_max_thresh=parameters["cts_sum_max_thresh"]
+        # read params based on used defined values
+        cts_sum_min_thresh=parameters["counts_sum_min_thresh"]
+        cts_sum_max_thresh=parameters["counts_sum_max_thresh"]
         max_jitter=args.max_jitter
         negative_sampling_ratio=args.negative_sampling_ratio
         add_revcomp=True
+        shuffle_at_epoch_start=True
+
     elif mode=="valid":
-        # no jitter at valid time - we are testing only at summits
-        # no reverse complementation at valid time
-        # fixed negative test for validation
-        nonpeak_regions=nonpeak_regions.sample(frac=args.negative_sampling_ratio, replace=False, random_state=args.seed)
-        cts_sum_min_thresh=parameters["cts_sum_min_thresh"]
-        cts_sum_max_thresh=parameters["cts_sum_max_thresh"]
+        # do not jitter at valid time - we are testing only at summits
         max_jitter=0
+        # no reverse complementation at valid time
         add_revcomp=False
+        # fix negative test for validation
+        nonpeak_regions=nonpeak_regions.sample(frac=args.negative_sampling_ratio, replace=False, random_state=args.seed)
         negative_sampling_ratio=1.0 # already subsampled
+        # filter outliers at validation - we dont want the model checkpoint to be sensitive to outliers
+        cts_sum_min_thresh=parameters["counts_sum_min_thresh"]
+        cts_sum_max_thresh=parameters["counts_sum_max_thresh"]
+        # no need to shuffle
+        shuffle_at_epoch_start=False
+
     elif mode=="test":
         # no jitter at valid time - we are testing only at summits
-        # no reverse complementation at valid time
-        # no subsampling of negatives - test on all positives and negatives
-        # no filtering of data points
         max_jitter=0
+        # no reverse complementation at valid time
         add_revcomp=False
+        # no subsampling of negatives - test on all positives and negatives
         negative_sampling_ratio=1.0
-        cts_sum_min_thresh="None" 
+        # filtering of data points based on outliers - need to discuss how to do this
+        cts_sum_min_thresh="None"
         cts_sum_max_thresh="None"
+        # no need to shuffle
+        shuffle_at_epoch_start=False
     else:
-        print("mode not defined - only train, test, valid are allowed")
-    return cts_sum_min_thresh,cts_sum_max_thresh,max_jitter,add_revcomp,nonpeak_regions,negative_sampling_ratio
+        print("mode not defined - only train, valid, test are allowed")
 
-def initialize_generators(args, mode, generator_name, parameters, return_coords):
+    return cts_sum_min_thresh,cts_sum_max_thresh,max_jitter,add_revcomp,nonpeak_regions,negative_sampling_ratio, shuffle_at_epoch_start
+
+def initialize_generators(args, mode, parameters, return_coords):
 
     assert(args.inputlen%2==0)
     assert(args.outputlen%2==0)
 
-    #defaults
+    # defaults
     peak_regions=None
     nonpeak_regions=None
 
@@ -57,43 +67,25 @@ def initialize_generators(args, mode, generator_name, parameters, return_coords)
         nonpeak_regions=pd.read_csv(args.nonpeaks,header=None,sep='\t',names=NARROWPEAK_SCHEMA)
         nonpeak_regions, chroms=splits.get_bed_regions_for_fold_split(nonpeak_regions, args.fold, mode) 
 
-    cts_sum_min_thresh,cts_sum_max_thresh,max_jitter,add_revcomp,nonpeak_regions, negative_sampling_ratio =  fetch_values_based_on_mode(mode, args, parameters, nonpeak_regions)
-  
-    if generator_name=="tiledb":
-        generator=tiledb_generator.TiledbGenerator(
-                                        peak_regions=peak_regions,
-                                        nonpeak_regions=nonpeak_regions,
-                                        genome_fasta=args.genome,
-                                        batch_size=args.batch_size,
-                                        input_len=args.inputlen,      
-                                        output_len=args.outputlen,
-                                        max_jitter=max_jitter,
-                                        negative_sampling_ratio=negative_sampling_ratio,
-                                        cts_sum_min_thresh=cts_sum_min_thresh,
-                                        cts_sum_max_thresh=cts_sum_max_thresh,
-                                        tdb_array=args.tdb_array,
-                                        tdb_dataset=args.tdb_dataset,
-                                        seed=args.seed,
-                                        add_revcomp=add_revcomp,
-                                        return_coords=return_coords
-                                        )
+    cts_sum_min_thresh, cts_sum_max_thresh, max_jitter, add_revcomp, nonpeak_regions, negative_sampling_ratio, shuffle_at_epoch_start =  \
+                                                                            fetch_params_based_on_mode(mode, args, parameters, nonpeak_regions)
 
-    if generator_name=="batchgen":
-        generator=batchgen_generator.ChromBPNetBatchGenerator(
-                                        peak_regions=peak_regions,
-                                        nonpeak_regions=nonpeak_regions,
-                                        genome_fasta=args.genome,
-                                        batch_size=args.batch_size,
-                                        inputlen=args.inputlen,                                        
-                                        outputlen=args.outputlen,
-                                        max_jitter=max_jitter,
-                                        negative_sampling_ratio=negative_sampling_ratio,
-                                        cts_sum_min_thresh=cts_sum_min_thresh,
-                                        cts_sum_max_thresh=cts_sum_max_thresh,
-                                        cts_bw_file=args.bigwig,
-                                        seed=args.seed,
-                                        add_revcomp=add_revcomp,
-                                        return_coords=return_coords
-                                        )
+    generator=batchgen_generator.ChromBPNetBatchGenerator(
+                                    peak_regions=peak_regions,
+                                    nonpeak_regions=nonpeak_regions,
+                                    genome_fasta=args.genome,
+                                    batch_size=args.batch_size,
+                                    inputlen=args.inputlen,                                        
+                                    outputlen=args.outputlen,
+                                    max_jitter=max_jitter,
+                                    negative_sampling_ratio=negative_sampling_ratio,
+                                    cts_sum_min_thresh=cts_sum_min_thresh,
+                                    cts_sum_max_thresh=cts_sum_max_thresh,
+                                    cts_bw_file=args.bigwig,
+                                    seed=args.seed,
+                                    add_revcomp=add_revcomp,
+                                    return_coords=return_coords,
+                                    shuffle_at_epoch_start=shuffle_at_epoch_start
+                                    )
     
     return generator
