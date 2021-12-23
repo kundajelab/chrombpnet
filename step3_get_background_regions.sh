4dcf159d5f2e6ea35c0174b4416f317b37022446
Anusri Pampari <p.anushri25@gmail.com>
2021-12-22 22:43:11 -0800

    adapted to the new background regions setup

--- step3_get_background_regions.sh
+++ step3_get_background_regions.sh
@@ -5,6 +5,7 @@ overlap_peak=$4
 inputlen=$5
 genomewide_gc=$6
 output_dir=$7
+fold=$8
 
 # We also slop both our blacklist and peak set with the inputlen//2 and merge them. We will use this combined set as exlusion bed.
 # we slop to make sure that the regions we choose have no intersection (not even 1bp) with the overlap peaks/blacklist regions
@@ -18,7 +19,7 @@ exclude_bed=$output_dir/exclude.bed
 
 # create regions of size inputlen that do not fall in exclude bed and that gc-match with the given overlap peaks
 # create as many regions as there are in overlap peaks bed file
-bash $PWD/src/helpers/make_gc_matched_negatives/run.sh $overlap_peak $exclude_bed $inputlen $output_dir $reference_fasta $genomewide_gc
+bash $PWD/src/helpers/make_gc_matched_negatives/run.sh $overlap_peak $exclude_bed $inputlen $output_dir $reference_fasta $genomewide_gc $fold $chrom_sizes
 # make a dummy summit file - rest of the pipeline uses this format
 awk -v OFS="\t" '{print $1, $2, $3, ".",  ".", ".", ".", ".", ".", "1057"}' $output_dir/negatives.bed > $output_dir/negatives_with_summit.bed
 
