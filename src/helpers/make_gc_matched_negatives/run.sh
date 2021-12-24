b7536e2bb76650e75ddd306936997cac3b41fbf1
Anusri Pampari <p.anushri25@gmail.com>
2021-12-23 17:59:45 -0800

    added log files till bias model training

--- src/helpers/make_gc_matched_negatives/run.sh
+++ src/helpers/make_gc_matched_negatives/run.sh
@@ -8,23 +8,55 @@ genome=$5
 genomewide_gc=$6
 fold=$7
 chrom_sizes=$8
+logfile=$9
 
-echo "get gc content of the positive sequences" 
+function timestamp {
+    # Function to get the current time with the new line character
+    # removed 
+    
+    # current time
+    date +"%Y-%m-%d_%H-%M-%S" | tr -d '\n'
+}
+
+# create the log file
+if [ -z "$logfile" ]
+  then
+    echo "No logfile supplied - creating one"
+    logfile=$output_prefix"_make_gc_matching_run.log"
+    touch $logfile
+fi
+
+echo $( timestamp ): "get gc content of the positive sequences" | tee -a $logfile
+echo $( timestamp ): "python $PWD/src/helpers/make_gc_matched_negatives/get_gc_content.py \\
+        --input_bed $foreground_bed \\
+        --genome $genome \\
+        --chrom_sizes $chrom_sizes \\
+        --output_prefix $output_dir/foreground.gc \\
+        --inputlen $inputlen" | tee -a $logfile
+
 python $PWD/src/helpers/make_gc_matched_negatives/get_gc_content.py \
         --input_bed $foreground_bed \
         --genome $genome \
         --chrom_sizes $chrom_sizes \
         --output_prefix $output_dir/foreground.gc \
-        --inputlen $inputlen \
+        --inputlen $inputlen | tee -a $logfile
 
-echo "get candidate negative bed" 
+echo $( timestamp ): "get candidate negative bed" | tee -a $logfile
+echo $( timestamp ): "bedtools intersect -v -a $genomewide_gc -b $exclude_bed  > $output_dir/candidate.negatives.bed" | tee -a $logfile
 bedtools intersect -v -a $genomewide_gc -b $exclude_bed  > $output_dir/candidate.negatives.bed
 
-echo "find regions in candidate negative bed that gc-match wth foreground" 
+echo $( timestamp ): "find regions in candidate negative bed that gc-match wth foreground" | tee -a $logfile
+echo $( timestamp ): "python $PWD/src/helpers/make_gc_matched_negatives/get_gc_matched_negatives.py \\
+        --candidate_negatives $output_dir/candidate.negatives.bed \\
+        --foreground_gc_bed $output_dir/foreground.gc.bed \\
+        --output_prefix $output_dir/negatives \\
+        --chr_fold_path $fold \\
+        --neg_to_pos_ratio_train 2" | tee -a $logfile
 python $PWD/src/helpers/make_gc_matched_negatives/get_gc_matched_negatives.py \
         --candidate_negatives $output_dir/candidate.negatives.bed \
         --foreground_gc_bed $output_dir/foreground.gc.bed \
         --output_prefix $output_dir/negatives \
         --chr_fold_path $fold \
-        --neg_to_pos_ratio_train 2 \
+        --neg_to_pos_ratio_train 2 | tee -a $logfile
+
 
--- src/helpers/preprocessing/bam_to_bigwig.sh
+++ src/helpers/preprocessing/bam_to_bigwig.sh
@@ -1,31 +1,60 @@
 #!/bin/bash
 
 input_bam=$1
-output_dir=$2
+output_prefix=$2
 data_type=$3
 chrom_sizes=$4
+logfile=$5
+
+function timestamp {
+    # Function to get the current time with the new line character
+    # removed 
+    
+    # current time
+    date +"%Y-%m-%d_%H-%M-%S" | tr -d '\n'
+}
+
+# create the log file
+if [ -z "$logfile" ]
+  then
+    echo "No logfile supplied - creating one"
+    logfile=$output_prefix"_bam_to_bigwig.log"
+    touch $logfile
+fi
+
 
 if [ "$data_type" = "DNASE_SE" ] ; then
-    echo "shift DNASE SE data"
-    samtools view -b -@50 -F780 -q30  $input_bam | bedtools bamtobed -i stdin | awk -v OFS="\t" '{if ($6=="-"){print $1,$2,$3+1,$4,$5,$6} else if ($6=="+") {print $1,$2,$3,$4,$5,$6}}' | bedtools genomecov -bg -5 -i stdin -g $chrom_sizes | bedtools sort -i stdin > $output_dir/tmp2
-    bedGraphToBigWig $output_dir/tmp2 $chrom_sizes $output_dir/shifted.sorted.bam.chrombpnet.unstranded.bw
-    rm $output_dir/tmp2
+    echo $( timestamp ): "shift DNASE SE data" | tee -a $logfile
+    echo $( timestamp ): "samtools view -b -@50 -F780 -q30  $input_bam | bedtools bamtobed -i stdin | awk -v OFS=\"\t\" \'{if (\$6==\"-\"){print \$1,\$2,\$3+1,\$4,\$5,\$6} else if (\$6==\"+\") {print \$1,\$2,\$3,\$4,\$5,\$6}}\' | bedtools genomecov -bg -5 -i stdin -g  $chrom_sizes | bedtools sort -i stdin > $output_prefix.tmp2" | tee -a $logfile
+    samtools view -b -@50 -F780 -q30  $input_bam | bedtools bamtobed -i stdin | awk -v OFS="\t" '{if ($6=="-"){print $1,$2,$3+1,$4,$5,$6} else if ($6=="+") {print $1,$2,$3,$4,$5,$6}}' | bedtools genomecov -bg -5 -i stdin -g $chrom_sizes | bedtools sort -i stdin > $output_prefix".tmp2"
+    echo $( timestamp ): "bedGraphToBigWig $output_prefix.tmp2 $chrom_sizes $output_prefix\_unstranded.bw" | tee -a $logfile
+    bedGraphToBigWig $output_prefix".tmp2" $chrom_sizes $output_prefix"_unstranded.bw"
+    echo $( timestamp ): "rm $output_prefix.tmp2" | tee -a $logfile
+    rm $output_prefix".tmp2"
 elif [ "$data_type" = "DNASE_PE" ] ; then
-    echo "shift DNASE PE data"
-    samtools view -b -@50 $input_bam | bedtools bamtobed -i stdin | awk -v OFS="\t" '{if ($6=="-"){print $1,$2,$3+1,$4,$5,$6} else if ($6=="+") {print $1,$2,$3,$4,$5,$6}}' | bedtools genomecov -bg -5 -i stdin -g $chrom_sizes | bedtools sort -i stdin > $output_dir/tmp2
-    bedGraphToBigWig $output_dir/tmp2 $chrom_sizes $output_dir/shifted.sorted.bam.chrombpnet.unstranded.bw
-    rm $output_dir/tmp2
+    echo $( timestamp ): "shift DNASE PE data" | tee -a $logfile
+    echo $( timestamp ): "samtools view -b -@50 $input_bam | bedtools bamtobed -i stdin | awk -v OFS=\"\t\" \'{if (\$6==\"-\"){print \$1,\$2,\$3+1,\$4,\$5,\$6} else if (\$6==\"+\") {print \$1,\$2,\$3,\$4,\$5,\$6}}\' | bedtools genomecov -bg -5 -i stdin -g  $chrom_sizes | bedtools sort -i stdin > $output_prefix.tmp2" | tee -a $logfile
+    samtools view -b -@50 $input_bam | bedtools bamtobed -i stdin | awk -v OFS="\t" '{if ($6=="-"){print $1,$2,$3+1,$4,$5,$6} else if ($6=="+") {print $1,$2,$3,$4,$5,$6}}' | bedtools genomecov -bg -5 -i stdin -g $chrom_sizes | bedtools sort -i stdin > $output_prefix".tmp2"
+    echo $( timestamp ): "bedGraphToBigWig $output_prefix.tmp2 $chrom_sizes $output_prefix\_unstranded.bw" | tee -a $logfile
+    bedGraphToBigWig $output_prefix".tmp2" $chrom_sizes $output_prefix"_unstranded.bw"
+    echo $( timestamp ): "rm $output_prefix.tmp2" | tee -a $logfile
+    rm $output_prefix".tmp2"
 elif [ "$data_type" = "ATAC_PE" ] ; then
-    echo "shift ATAC PE data"
-    samtools view -b -@50 $input_bam | bedtools bamtobed -i stdin | awk -v OFS="\t" '{if ($6=="-"){print $1,$2,$3-4,$4,$5,$6} else if ($6=="+") {print $1,$2+4,$3,$4,$5,$6}}' | bedtools genomecov -bg -5 -i stdin -g $chrom_sizes | bedtools sort -i stdin > $output_dir/tmp2
-    bedGraphToBigWig $output_dir/tmp2 $chrom_sizes $output_dir/shifted.sorted.bam.chrombpnet.unstranded.bw
-    rm $output_dir/tmp2
+    echo $( timestamp ): "shift ATAC PE data" | tee -a $logfile
+    echo $( timestamp ): "samtools view -b -@50 $input_bam | bedtools bamtobed -i stdin | awk -v OFS=\"\t\" \'{if (\$6==\"-\"){print \$1,\$2,\$3-4,\$4,\$5,\$6} else if (\$6==\"+\") {print \$1,\$2+4,\$3,\$4,\$5,\$6}}\' | bedtools genomecov -bg -5 -i stdin -g  $chrom_sizes | bedtools sort -i stdin > $output_prefix.tmp2" | tee -a $logfile
+    samtools view -b -@50 $input_bam | bedtools bamtobed -i stdin | awk -v OFS="\t" '{if ($6=="-"){print $1,$2,$3-4,$4,$5,$6} else if ($6=="+") {print $1,$2+4,$3,$4,$5,$6}}' | bedtools genomecov -bg -5 -i stdin -g $chrom_sizes | bedtools sort -i stdin > $output_prefix".tmp2"
+    echo $( timestamp ): "bedGraphToBigWig $output_prefix.tmp2 $chrom_sizes $output_prefix\_unstranded.bw" | tee -a $logfile
+    bedGraphToBigWig $output_prefix".tmp2" $chrom_sizes $output_prefix"_unstranded.bw"
+    echo $( timestamp ): "rm $output_prefix.tmp2" | tee -a $logfile
+    rm $output_prefix".tmp2"
 elif [ "$data_type" = "ATAC_SE" ] ; then
-    echo "shift ATAC SE data"
-    samtools view -b -@50 -F780 -q30  $input_bam | bedtools bamtobed -i stdin | awk -v OFS="\t" '{if ($6=="-"){print $1,$2,$3-4,$4,$5,$6} else if ($6=="+") {print $1,$2+4,$3,$4,$5,$6}}' | bedtools genomecov -bg -5 -i stdin -g $chrom_sizes | bedtools sort -i stdin > $output_dir/tmp2
-    bedGraphToBigWig $output_dir/tmp2 $chrom_sizes $output_dir/shifted.sorted.bam.chrombpnet.unstranded.bw
-    rm $output_dir/tmp2
+    echo $( timestamp ): "shift ATAC SE data" | tee -a $logfile
+    echo $( timestamp ): "samtools view -b -@50 -F780 -q30  $input_bam | bedtools bamtobed -i stdin | awk -v OFS=\"\t\" \'{if (\$6==\"-\"){print \$1,\$2,\$3-4,\$4,\$5,\$6} else if (\$6==\"+\") {print \$1,\$2+4,\$3,\$4,\$5,\$6}}\' | bedtools genomecov -bg -5 -i stdin -g  $chrom_sizes | bedtools sort -i stdin > $output_prefix.tmp2" | tee -a $logfile
+    samtools view -b -@50 -F780 -q30  $input_bam | bedtools bamtobed -i stdin | awk -v OFS="\t" '{if ($6=="-"){print $1,$2,$3-4,$4,$5,$6} else if ($6=="+") {print $1,$2+4,$3,$4,$5,$6}}' | bedtools genomecov -bg -5 -i stdin -g $chrom_sizes | bedtools sort -i stdin > $output_prefix".tmp2"
+    echo $( timestamp ): "bedGraphToBigWig $output_prefix.tmp2 $chrom_sizes $output_prefix\_unstranded.bw" | tee -a $logfile
+    bedGraphToBigWig $output_prefix".tmp2" $chrom_sizes $output_prefix"_unstranded.bw"
+    echo $( timestamp ): "rm $output_prefix.tmp2" | tee -a $logfile
+    rm $output_prefix".tmp2"
 else
-    echo "unknown data type"
-    echo $data_type
+    echo "ERROR: unknown data type " $data_type | tee -a $logfile
 fi
