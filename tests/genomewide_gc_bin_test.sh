36198686451dbcc34bf2657344da5bb275affac8
Ubuntu <ubuntu@ip-10-14-153-142.us-west-2.compute.internal>
2022-02-22 19:13:41 +0000

    updated unit test with correct expected md5sum values

--- tests/genomewide_gc_bin_test.sh
+++ tests/genomewide_gc_bin_test.sh
@@ -13,9 +13,9 @@ do
 done
 
 #assert we get the expectd md5sum values
-#2911f95c85673d86cff8daa4eaca5250  100.chr21.gc.bed
-#0bec3a55b97a2fa1a4f62e5abb375653  1000.chr21.gc.bed
-#43c3d7185c4748532b36959f3c0ca346  500.chr21.gc.bed
+expected_md5sum_100="1c49d5ff0f5b8e8c09d38360fc50561c"
+expected_md5sum_500="f99b40005b496a8f4f7fa9ca04e89bb4"
+expected_md5sum_1000="b1c5b8157ff49a9365f0dd6c628afc98"
 
 #get md5sum values for the genomewide gc content
 md5sum_100=($(md5sum 100.chr21.gc.bed))
@@ -25,27 +25,27 @@ echo $md5sum_100
 echo $md5sum_500
 echo $md5sum_1000
 
-if [ $md5sum_100 = "2911f95c85673d86cff8daa4eaca5250" ]; 
+if [ $md5sum_100 = $expected_md5sum_100 ]; 
 then
     echo "obtained expected md5sum for 100.chr21.gc.bed"
 else
-    echo "md5sum $md5sum_100 does not match expected value 2911f95c85673d86cff8daa4eaca5250"
+    echo "md5sum $md5sum_100 does not match expected value $expected_md5sum_100"
     exit 1
 fi
 
-if [ $md5sum_500 = "43c3d7185c4748532b36959f3c0ca346" ];
+if [ $md5sum_500 = $expected_md5sum_500 ];
 then
     echo "obtained expected md5sum for 500.chr21.gc.bed"
 else
-    echo "md5sum $md5sum_500 does not match expected value 43c3d7185c4748532b36959f3c0ca346"
+    echo "md5sum $md5sum_500 does not match expected value $expected_md5sum_500"
     exit 1
 fi
 
-if [ $md5sum_1000 = "0bec3a55b97a2fa1a4f62e5abb375653" ]; 
+if [ $md5sum_1000 = $expected_md5sum_1000 ]; 
 then
-    echo "obtained expected md5sum for 100.chr21.gc.bed"
+    echo "obtained expected md5sum for 1000.chr21.gc.bed"
 else
-    echo "md5sum $md5sum_100 does not match expected value 0bec3a55b97a2fa1a4f62e5abb375653"
+    echo "md5sum $md5sum_100 does not match expected value $expected_md5sum_1000"
     exit 1
 fi
 
