c53a3291d5ac90015774c9c6646f1f12fb76141e
Ubuntu <ubuntu@ip-10-14-153-142.us-west-2.compute.internal>
2022-06-20 20:39:56 +0000

    updated parameter wraps in modisco/run.sh

--- chrombpnet/evaluation/modisco/run.sh
+++ chrombpnet/evaluation/modisco/run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # exit when any command fails
 set -e
 
@@ -7,16 +9,16 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 
-scores_prefix=$1
-output_dir=$2
-score_type=$3
-seqlets=$4
-crop=$5
-meme_db=$6
-meme_logos=$7
-vier_logos=$8
-vier_html=$9
-html_link=${10}
+scores_prefix=${1?param missing - scores_prefix}
+output_dir=${2?param missing - output_dir}
+score_type=${3?param missing - score_type}
+seqlets=${4?param missing - seqlets}
+crop=${5?param missing - crop} 
+meme_db=${6?param missing - meme_db}
+meme_logos=${7?param missing - meme_logos}
+vier_logos=${8?param_missing - vier_logos}
+vier_html=${9?param_missing - vier_html}
+html_link=${10?param_missing - html_link}
 
 python run_modisco.py -s $scores_prefix -p $score_type -o $output_dir -m $seqlets -c $crop
 python fetch_tomtom.py -m $output_dir/modisco_results_allChroms_counts.hdf5 -o $output_dir/$score_type.tomtom.tsv -d $meme_db -n 10 -th 0.3
