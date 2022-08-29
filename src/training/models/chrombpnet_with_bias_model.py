bd5130e73ccdf99c55dc276a81958452ff1831b5
Anusri Pampari <p.anushri25@gmail.com>
2022-08-29 12:51:06 -0700

    symmteric cropping already done- commenting check

--- src/training/models/chrombpnet_with_bias_model.py
+++ src/training/models/chrombpnet_with_bias_model.py
@@ -68,7 +68,7 @@ def bpnet_model(filters, n_dil_layers, sequence_len, out_pred_len):
     # Step 1.2 - Crop to match size of the required output size
     cropsize = int(int_shape(prof_out_precrop)[1]/2)-int(out_pred_len/2)
     assert cropsize>=0
-    assert (cropsize % 2 == 0) # Necessary for symmetric cropping
+    #assert (cropsize % 2 == 0) # Necessary for symmetric cropping
 
     prof = Cropping1D(cropsize,
                 name='wo_bias_bpnet_logitt_before_flatten')(prof_out_precrop)
