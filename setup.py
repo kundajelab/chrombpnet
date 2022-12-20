baa6eab53d8dae27231871b7619567b5d10c726e
Anusri <anusri@stanford.edu>
2022-12-20 08:25:41 +0000

    removing tutorial and gc matched negatives from setup

--- setup.py
+++ setup.py
@@ -14,28 +14,16 @@ config = {
     'include_package_data': True,
     'description': 'chrombpnet predicts chromatin accessibility from sequence',
     'download_url': 'https://github.com/kundajelab/chrombpnet',
-    'version': '1.3',
+    'version': '0.1.8',
     'packages': find_packages(),
     'python_requires': '>=3.8',
     'install_requires': install_requires,
     'zip_safe': False,
-    'scripts':['chrombpnet/helpers/make_gc_matched_negatives/make_gc_matched_negatives.sh',
+    'scripts':[
                'chrombpnet/training/models/bpnet_model.py',
-               'chrombpnet/training/models/chrombpnet_with_bias_model.py',
-               'workflows/tutorial/step1_download_bams_and_peaks.sh',
-               'workflows/tutorial/step2_make_bigwigs_from_bams.sh',
-               'workflows/tutorial/step3_get_background_regions.sh',
-               'workflows/tutorial/step4_train_bias_model.sh',
-               'workflows/tutorial/step5_interpret_bias_model.sh',
-               'workflows/tutorial/step6_train_chrombpnet_model.sh',
-               'workflows/tutorial/step7_interpret_chrombpnet_model.sh',
-               'workflows/train_bias_model.sh',
-               'workflows/train_chrombpnet_model.sh'
+               'chrombpnet/training/models/chrombpnet_with_bias_model.py'
     ],
     'entry_points': {'console_scripts': [
-        'chrombpnet_genomewide_gc = chrombpnet.helpers.make_gc_matched_negatives.get_genomewide_gc_buckets.get_genomewide_gc_bins:main',
-        'chrombpnet_gc_content_foreground = chrombpnet.helpers.make_gc_matched_negatives.get_gc_content:main',
-        'chrombpnet_gc_matched_negatives = chrombpnet.helpers.make_gc_matched_negatives.get_gc_matched_negatives:main',
         'chrombpnet_make_splits = chrombpnet.helpers.make_chr_splits.splits:main',
         'chrombpnet = chrombpnet.CHROMBPNET:main',
         'print_meme_motif_file = chrombpnet.data.__init__:print_meme_motif_file']}
