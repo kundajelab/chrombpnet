93aaf2d51e2f01c09c664c64095af701641a4999
Anusri <anusri@stanford.edu>
2023-03-24 22:44:08 +0000

    updated version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ config = {
     'include_package_data': True,
     'description': 'chrombpnet predicts chromatin accessibility from sequence',
     'download_url': 'https://github.com/kundajelab/chrombpnet',
-    'version': '0.1.1',
+    'version': '0.1.2',
     'packages': find_packages(),
     'python_requires': '>=3.8',
     'install_requires': install_requires,
