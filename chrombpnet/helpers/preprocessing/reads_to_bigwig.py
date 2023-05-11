659c9e9ff9fe6ef0cf7e201b08b51ee7a275f727
Surag Nair <10693069+suragnair@users.noreply.github.com>
2023-05-11 14:27:21 -0700

    Revert to unix sort in reads_to_bigwig.py
    related to #104 and change in https://github.com/kundajelab/chrombpnet/commit/027accba991dbb7290d91bfcc96baead0a9ec8a2

--- chrombpnet/helpers/preprocessing/reads_to_bigwig.py
+++ chrombpnet/helpers/preprocessing/reads_to_bigwig.py
@@ -37,7 +37,7 @@ def generate_bigwig(input_bam_file, input_fragment_file, input_tagalign_file, ou
     elif input_tagalign_file:
         p1 = auto_shift_detect.tagalign_stream(input_tagalign_file)
 
-    cmd = """awk -v OFS="\\t" '{{if ($6=="+"){{print $1,$2{0:+},$3,$4,$5,$6}} else if ($6=="-") {{print $1,$2,$3{1:+},$4,$5,$6}}}}' | sort -k1,1 | bedtools genomecov -bg -5 -i stdin -g {2} | bedtools sort -i stdin """.format(plus_shift_delta, minus_shift_delta, chrom_sizes_file)
+    cmd = """awk -v OFS="\\t" '{{if ($6=="+"){{print $1,$2{0:+},$3,$4,$5,$6}} else if ($6=="-") {{print $1,$2,$3{1:+},$4,$5,$6}}}}' | sort -k1,1 | bedtools genomecov -bg -5 -i stdin -g {2} | sort -k1,1 -k2,2n""".format(plus_shift_delta, minus_shift_delta, chrom_sizes_file)
 
     tmp_bedgraph = tempfile.NamedTemporaryFile()
     print("Making BedGraph")
