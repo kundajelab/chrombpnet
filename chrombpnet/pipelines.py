c4ccf711d1d74be2631b66df83d6b46461373253
Austin Wang <austin.wang1357@gmail.com>
2023-06-04 22:36:37 -0700

    Update pipelines.py

--- chrombpnet/pipelines.py
+++ chrombpnet/pipelines.py
@@ -16,8 +16,8 @@ def chrombpnet_train_pipeline(args):
 	# Shift bam and convert to bigwig
 	import chrombpnet.helpers.preprocessing.reads_to_bigwig as reads_to_bigwig	
 	args.output_prefix = os.path.join(args.output_dir,"auxiliary/{}data".format(fpx))
-	args.plus_shift = None
-	args.minus_shift = None
+	args.plus_shift = 0
+	args.minus_shift = 0
 	reads_to_bigwig.main(args)
 	
 	# QC bigwig
