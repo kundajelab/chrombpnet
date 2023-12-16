a5c231fdf231bb29e9ca53d42a4c6e196f7546e8
Anusri <anusri@stanford.edu>
2023-12-16 18:08:07 +0000

    fixed typo issue 165 and None error in regions used

--- chrombpnet/evaluation/make_bigwigs/bigwig_helper.py
+++ chrombpnet/evaluation/make_bigwigs/bigwig_helper.py
@@ -38,10 +38,10 @@ def get_regions(regions_file, seqlen, regions_used=None):
 
     regions = pd.read_csv(regions_file,sep='\t',header=None)
     #print(regions)
-    if regions_used:
-        regions = [[x[0], int(x[1])+int(x[9])-seqlen//2, int(x[1])+int(x[9])+seqlen//2, int(x[1])+int(x[9])] for x in np.array(regions.values)[regions_used]]
-    else:
+    if regions_used is None:
         regions = [[x[0], int(x[1])+int(x[9])-seqlen//2, int(x[1])+int(x[9])+seqlen//2, int(x[1])+int(x[9])] for x in np.array(regions.values)]
+    else:
+        regions = [[x[0], int(x[1])+int(x[9])-seqlen//2, int(x[1])+int(x[9])+seqlen//2, int(x[1])+int(x[9])] for x in np.array(regions.values)[regions_used]]
 
     return regions
 
