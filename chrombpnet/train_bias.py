5a08e5b1352e7066c68a65f3bcf97b79f39d6bfc
ziwei <ziwei75@stanford.edu>
2024-01-29 11:18:28 -0800

    train script

---
+++ chrombpnet/train_bias.py
@@ -0,0 +1,19 @@
+import parsers as parsers
+import os
+from data import DefaultDataFile, get_default_data_path
+from data import print_meme_motif_file
+import pipelines as pipelines
+import copy
+import pandas as pd
+import logging
+logging.getLogger('matplotlib.font_manager').disabled = True
+	
+
+args = parsers.read_parser()
+os.makedirs(os.path.join(args.output_dir,"logs"), exist_ok=True)
+os.makedirs(os.path.join(args.output_dir,"auxiliary"), exist_ok=True)
+os.makedirs(os.path.join(args.output_dir,"models"), exist_ok=True)
+os.makedirs(os.path.join(args.output_dir,"evaluation"), exist_ok=True)
+
+pipelines.train_bias_pipeline(args)
+		
